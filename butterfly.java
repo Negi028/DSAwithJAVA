f3860ab30fa887bb5a7a0b0e943bb3e28ca30d25
Akash Negi <akashnegi449@gmail.com>
2025-02-16 19:24:46 +0530

    butterfly by nested loop note:space logic

---
+++ butterfly.java
@@ -0,0 +1,29 @@
+public class butterfly {
+    public static void main(String[] args) {
+        for(int row=1;row<=4;row++){
+            for(int col=1;col<=row;col++){
+                System.out.print("*");
+            }
+            for(int space=1;space<=2*(4-row);space++){
+                System.out.print(" ");
+            }
+            for(int col=1;col<=row;col++){
+                System.out.print("*");
+            }
+            System.out.println();
+           
+        }
+        for(int row2=4;row2>=1;row2--){
+            for(int col=1;col<=row2;col++){
+                System.out.print("*");
+            }
+            for(int space2=1;space2<2*(4-row2);space2++){
+                System.out.print(" ");
+            }
+            for(int col=1;col<=row2;col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
