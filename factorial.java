955232ecb320cd9d08798a919121456cec93da9f
Akash Negi <akashnegi449@gmail.com>
2025-02-18 23:07:16 +0530

    factorial function

---
+++ factorial.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+// factorial function : n*(n-1)*(n-2)....: o!=1
+
+public class factorial {
+    public static void printFact(int n){
+        int fact=1;
+        //loops
+        for(int i=n;i>=1;i--){
+            fact=fact*i;
+        }
+        System.out.println(fact);
+        return;//void dont need to return statement by default it return statement
+
+    }
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+        printFact(n);
+        
+    }
+}
