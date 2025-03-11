fef90c1f15be590b8e75a7ca727e8ce5b7f7f981
Akash Negi <akashnegi449@gmail.com>
2025-03-12 00:22:56 +0530

    function without return type

---
+++ Func_tion.java
@@ -0,0 +1,10 @@
+//with no return type or empty
+public class Func_tion {
+    public static void main(String[] args) {
+        Func_tion mf=new Func_tion();
+        mf.calculateArea(5, 2);
+    }
+    public void calculateArea(int length,int breadth){
+        System.out.println("area is "+length*breadth);
+    }
+}
